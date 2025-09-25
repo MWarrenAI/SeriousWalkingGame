17d7bad1a40b917fe8d76c499311fb443c8f75cf
Hallam Rear <hallamrear@gmail.com>
2025-09-25 12:44:39 +0100

    Merged mark into me and cleaned up a little.

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -1,10 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.EnhancedTouch;
 using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    [RuntimeInitializeOnLoadMethodAttribute]
+    static void EnableInputSystem()
+    {
+        EnhancedTouchSupport.Enable();
+        TouchSimulation.Enable();
+    }
+
     public void MoveToScene(int sceneID)
     {
         SceneManager.LoadScene(sceneID);
